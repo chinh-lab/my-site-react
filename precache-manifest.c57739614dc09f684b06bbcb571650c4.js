17b6e295c0516804ea77d83c8da834f5705f3151
Chinh Le <chinh.le11@gmail.com>
2020-04-16 16:49:49 -0400

    Updates

---
+++ precache-manifest.c57739614dc09f684b06bbcb571650c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5126ef308f6d16b84e3fdb2f407e2073",
+    "url": "/my-site-react/index.html"
+  },
+  {
+    "revision": "2da16142ce02221d0f9b",
+    "url": "/my-site-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c85639ab7199b05519e0",
+    "url": "/my-site-react/static/js/2.83a114d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-site-react/static/js/2.83a114d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2da16142ce02221d0f9b",
+    "url": "/my-site-react/static/js/main.3a8e16d8.chunk.js"
+  },
+  {
+    "revision": "9a1f9b1286b4cff5e371",
+    "url": "/my-site-react/static/js/runtime-main.4856022d.js"
+  }
+]);
